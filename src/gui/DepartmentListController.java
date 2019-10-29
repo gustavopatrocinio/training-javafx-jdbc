bedca73091013b6efdf773ae51fa252ed169b96b
Gustavo Patrocínio <gustavopatrocinio00@gmail.com>
2019-10-29 09:42:02 -0300

    DepartmentService

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -1,9 +1,12 @@
 package gui;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -12,9 +15,12 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import model.entities.Department;
+import model.services.DepartmentService;
 
 public class DepartmentListController implements Initializable {
 
+	private DepartmentService service;
+	
 	@FXML
 	private TableView<Department> tableViewDepartment;
 	
@@ -27,9 +33,17 @@ public class DepartmentListController implements Initializable {
 	@FXML
 	private Button btNew;
 	
+	//atributo para carregar os atributos na tableview
+	@FXML
+	private ObservableList<Department> obsList;
+	
 	@FXML
 	public void onBtNewAction() {
 		System.out.println("Click");
+	}
+	
+	public void setDepartmentService(DepartmentService service) {
+		this.service = service;
 	}
 	
 	@Override
@@ -45,6 +59,18 @@ public class DepartmentListController implements Initializable {
 		//macete para o tableview acompanhar a altura da janela
 		Stage stage = (Stage) Main.getMainScene().getWindow();
 		tableViewDepartment.prefHeightProperty().bind(stage.heightProperty());
+	}
+	
+	//método para acessar o serviço, carregar departamentos e jogá-los na observableList
+	public void updateTableView() {
+		if (service == null) {
+			throw new IllegalStateException("Service was null");
+		}
+		List<Department> list = service.findAll();
+		obsList = FXCollections.observableArrayList(list); // instanciando obsList pegando os dados da lista
+		tableViewDepartment.setItems(obsList); //carregar os itens na tableView
+		
+		
 	}
 
 }
